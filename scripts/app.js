3f91f4d8b10437eebf8a5bf178fab9bf960c2aca
Chottcodes <cwellshott@gmail.com>
2024-12-04 12:17:58 -0800

    update

---
+++ scripts/app.js
@@ -0,0 +1,17 @@
+let dailytextBTN=document.getElementById("daily");
+let weeklytextBTN=document.getElementById("weekly");
+let monthlytextBTN=document.getElementById("monthly");
+
+function getJSONData(){
+    
+}
+
+dailytextBTN.addEventListener('click',function(){
+    
+})
+weeklytextBTN.addEventListener('click',function(){
+    
+})
+monthlytextBTN.addEventListener('click',function(){
+    
+})
