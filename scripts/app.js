65adf4c3d5f74ee260ac8240a0a76f6166af77c5
Chottcodes <cwellshott@gmail.com>
2024-12-05 14:43:02 -0800

    update to js file

--- scripts/app.js
+++ scripts/app.js
@@ -14,8 +14,25 @@ let socialhrsTXT=document.getElementById("social-hrs");
 let socialprevious=document.getElementById("Social-previous-hrs");
 let selfcarehrsTXT=document.getElementById("self-care-hrs");
 let selfcareprevious=document.getElementById("self-care-previos-hrs");
-
-
+window.addEventListener("load",onLoad);
+function onLoad(){
+    workhrsTxt.innerText=datajson[0].timeframes.weekly.current+"hrs";
+    previoushoursworkTXT.innerText="Last Week - "+datajson[0].timeframes.weekly.previous+"hrs";
+    playhrsTXT.innerText=datajson[1].timeframes.weekly.current+"hrs";
+    playprevious.innerText="Last Week - "+datajson[1].timeframes.weekly.previous+"hrs";
+    studyhrsTXT.innerText=datajson[2].timeframes.weekly.current+"hrs";
+    studyprevious.innerText="Last Week - "+datajson[1].timeframes.weekly.previous+"hrs";
+    exercisehrsTXT.innerText=datajson[3].timeframes.weekly.current+"hrs";
+    exerciseprevious.innerText="Last Week - "+datajson[3].timeframes.weekly.previous+"hrs";
+    socialhrsTXT.innerText=datajson[4].timeframes.weekly.current+"hrs";
+    socialprevious.innerText="Last Week - "+datajson[4].timeframes.weekly.previous+"hrs";
+    selfcarehrsTXT.innerText=datajson[5].timeframes.weekly.current+"hrs";
+    selfcareprevious.innerText="Last Week - "+datajson[5].timeframes.weekly.previous+"hrs";
+    dailytextBTN.style.color="#7078c9";
+    weeklytextBTN.style.color="white";
+    monthlytextBTN.style.color="#7078c9";
+    console.log("hello");
+}
 dailytextBTN.addEventListener('click',function(){
     workhrsTxt.innerText=datajson[0].timeframes.daily.current+"hrs";
     previoushoursworkTXT.innerText="Last Week - "+datajson[0].timeframes.daily.previous+"hrs";
--- scripts/functions.js
+++ scripts/functions.js
@@ -1,8 +1,8 @@
+let datajson;
 async function getData() {
   const promise = await fetch("../data/data.json");
   const data = await promise.json();
   datajson = data;
 }
-let datajson = "";
 getData();
 export { datajson };
